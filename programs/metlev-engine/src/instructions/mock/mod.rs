e818b3f2de7566b9e89ff62b7cc641f6719afee4
0xRektified <bugislife@proton.me>
2026-02-24 03:05:16 +0800

    feat: files setup for oracle mock

---
+++ programs/metlev-engine/src/instructions/mock/mod.rs
@@ -0,0 +1,5 @@
+pub mod initialize_mock_oracle;
+pub mod update_mock_oracle;
+
+pub use initialize_mock_oracle::*;
+pub use update_mock_oracle::*;
--- programs/metlev-engine/src/state/mod.rs
+++ programs/metlev-engine/src/state/mod.rs
@@ -2,8 +2,10 @@ pub mod config;
 pub mod position;
 pub mod lending_vault;
 pub mod lp_position;
+pub mod mock_oracle;
 
 pub use config::*;
 pub use position::*;
 pub use lending_vault::*;
 pub use lp_position::*;
+pub use mock_oracle::*;
